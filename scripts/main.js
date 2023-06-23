8e96f969ba484034d61d9968b5fe9d7be6f29061
kdiepstraten <kodie@live.nl>
2023-06-23 11:53:45 +0200

    opdracht af

--- scripts/main.js
+++ scripts/main.js
@@ -72,3 +72,56 @@ const departments = {
 }
 
 console.log(departments);
+
+console.log(`De afdeling Sales heeft ${departments.sales.numberOfEmployees} medewerkers`);
+console.log(`Marketing is een leuke afdeling om te werken. ${departments.marketing.description}`);
+console.log(`De afdeling Customer Service heeft ${departments["customer-service"].numberOfEmployees} medewerkers`);
+console.log(`Sales is een uitdagende afdeling om te werken als Verkoopmanager. ${departments.sales.jobs[1].description}`)
+
+const result1 = prompt('Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]');
+
+
+switch (result1) {
+    case "marketing":
+        console.log(`${result1} is een leuke afdeling om te werken. Er werken op dit moment ${departments.marketing.numberOfEmployees} medewerkers. `)
+        break;
+    case "sales":
+        console.log(`${result1} is een leuke afdeling om te werken. Er werken op dit moment ${departments.sales.numberOfEmployees} medewerkers`);
+        break;
+    case "customer-service":
+        console.log(`${result1} is een leuke afdeling om te werken. Er werken op dit moment ${departments["customer-service"].numberOfEmployees} medewerkers`);
+        break;
+    default:
+        document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
+}
+
+const result = prompt(`Je koos ${result1}. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in.
+    0: ${departments[result1].jobs[0].title}
+    1: ${departments[result1].jobs[1].title}
+    2: ${departments[result1].jobs[2].title}
+    3: ${departments[result1].jobs[3].title}`);
+
+switch (result) {
+    case "0":
+        document.getElementById('role-title').textContent = `${departments[result1].jobs[result].title}`;
+        document.getElementById('department-description').textContent = `${departments.marketing.jobs[result].description}`;
+        document.getElementById('role-description').textContent = `${departments[result1].jobs[result].description}`;
+        break;
+    case "1":
+        document.getElementById('role-title').textContent = `${departments[result1].jobs[result].title}`;
+        document.getElementById('department-description').textContent = `${departments.marketing.jobs[result].description}`;
+        document.getElementById('role-description').textContent = `${departments[result1].jobs[result].description}`;
+        break;
+    case "2":
+        document.getElementById('role-title').textContent = `${departments[result1].jobs[result].title}`;
+        document.getElementById('department-description').textContent = `${departments.marketing.jobs[result].description}`;
+        document.getElementById('role-description').textContent = `${departments[result1].jobs[result].description}`;
+        break;
+    case "3":
+        document.getElementById('role-title').textContent = `${departments[result1].jobs[result].title}`;
+        document.getElementById('department-description').textContent = `${departments.marketing.jobs[result].description}`;
+        document.getElementById('role-description').textContent = `${departments[result1].jobs[result].description}`;
+        break;
+    default:
+        document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
+}
